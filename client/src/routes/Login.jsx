c92c54437e5e1596eb3219e13741c56337e5cf49
Alisson Silva dos Santos <92067558+alissons-repos@users.noreply.github.com>
2023-04-26 09:44:15 -0300

    atualizando a rota login

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -12,17 +12,17 @@ const Login = () => {
 	const location = useLocation();
 	const from = location.state?.from?.pathname || '/';
 
-	const userRef = useRef(); // Por o foco no campo usuário quando a tela recarregar
-	const errRef = useRef(); // Por o foco em erros para auxiliar na acessibilidade
+	const userRef = useRef(); // Necessário para por o foco no campo e-mail quando a tela recarregar
+	const errRef = useRef(); // Necessário para por o foco em erros para auxiliar na acessibilidade
 
 	const [user, setUser] = useState('');
 	const [pwd, setPwd] = useState('');
 	const [errMsg, setErrMsg] = useState('');
-	// const [success, setSuccess] = useState(false); // Esse estado só está aqui para mostrar uma mensagem de sucesso
+	const [success, setSuccess] = useState(false); // Necessário para mandar uma mensagem de sucesso e um placeholder de carregamento
 	// Devemos em seguida redirecionar o usuário para "dentro da aplicação"
 
 	useEffect(() => {
-		userRef.current.focus(); // Por o foco no input do usuário
+		userRef.current.focus(); // Por o foco no input de e-mail
 	}, []);
 
 	useEffect(() => {
@@ -42,17 +42,17 @@ const Login = () => {
 			setAuth({ user, accessToken });
 			setUser('');
 			setPwd('');
-			// setSuccess(true);
+			setSuccess(true);
 			navigate(from, { replace: true });
 		} catch (err) {
 			if (!err?.response) {
-				setErrMsg('Sem resposta do servidor');
+				setErrMsg('Sem resposta do servidor!');
 			} else if (err.response?.status === 400) {
-				setErrMsg('Está faltando o e-mail ou a senha');
+				setErrMsg('Está faltando o e-mail ou a senha!');
 			} else if (err.response?.status === 401) {
-				setErrMsg('Não autorizado');
+				setErrMsg('Não autorizado!');
 			} else {
-				setErrMsg('Login falhou');
+				setErrMsg('Algo deu errado!');
 			}
 			errRef.current.focus(); // Por o foco na mensagen de erro
 		}
@@ -67,47 +67,54 @@ const Login = () => {
 	}, [persist]);
 
 	return (
-		// {success ? (
-		// 	<section>
-		// 		<h1>Você está logado</h1>
-		// 		navigate('/feed');
-		// 		{/* Colocar um placeholder de carregamento aqui */}
-		// 	</section>
-		// ) : (
-		// 	<section>A SEÇÃO ABAIXO</section>
-		// )}
-		<section>
-			<p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live='assertive'>
-				{errMsg}
-			</p>
-			<h1>Login</h1>
-			<form onSubmit={handleSubmit}>
-				<label htmlFor='email'>E-mail:</label>
-				<input
-					type='email'
-					id='email'
-					autoComplete='off'
-					ref={userRef}
-					value={user} // Torna o valor do input controlável e assim podemos limpar o campo quando necessário
-					required
-					onChange={(e) => setUser(e.target.value)}
-				/>
-				<label htmlFor='password'>Senha:</label>
-				<input type='password' id='password' value={pwd} required onChange={(e) => setPwd(e.target.value)} />
-				<button type='submit'>Entrar</button>
-				<div className='persistCheck'>
-					<input type='checkbox' id='persist' onChange={togglePersist} checked={persist} />
-					<label htmlFor='persist'>Manter-se conectado</label>
-				</div>
-			</form>
-			<p>
-				Ainda não possui uma conta?
-				<br />
-				<span className='line'>
-					<Link to='/register'>Cadastre-se</Link>
-				</span>
-			</p>
-		</section>
+		<>
+			{success ? (
+				<section>
+					{/* Colocar um placeholder de carregamento aqui */}
+					<h1>Login efetuado com sucesso!</h1>
+					{navigate('/feed')};
+				</section>
+			) : (
+				<section>
+					<p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'}>
+						{errMsg}
+					</p>
+					<h1>Entrar</h1>
+					<form onSubmit={handleSubmit}>
+						<label htmlFor='email'>E-mail:</label>
+						<input
+							type='email'
+							id='email'
+							autoComplete='off'
+							ref={userRef}
+							value={user} // Torna o valor do input controlável e assim podemos limpar o campo quando necessário
+							required
+							onChange={(e) => setUser(e.target.value)}
+						/>
+						<label htmlFor='password'>Senha:</label>
+						<input
+							type='password'
+							id='password'
+							value={pwd}
+							required
+							onChange={(e) => setPwd(e.target.value)}
+						/>
+						<button type='submit'>Entrar</button>
+						<div className='persistCheck'>
+							<input type='checkbox' id='persist' onChange={togglePersist} checked={persist} />
+							<label htmlFor='persist'>Manter-se conectado</label>
+						</div>
+					</form>
+					<p>
+						Ainda não possui uma conta?
+						<br />
+						<span className='line'>
+							<Link to='/register'>Cadastre-se</Link>
+						</span>
+					</p>
+				</section>
+			)}
+		</>
 	);
 };
 
