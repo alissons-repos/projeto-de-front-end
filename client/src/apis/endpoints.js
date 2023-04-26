0cea92e06e344752d24a947e753cf227cad60e2a
Alisson Silva dos Santos <92067558+alissons-repos@users.noreply.github.com>
2023-04-26 10:57:05 -0300

    criando o arquivo endpoints

---
+++ client/src/apis/endpoints.js
@@ -0,0 +1,10 @@
+const routes = {
+	BASE_URL: 'http://localhost:8080/',
+	LOGIN_URL: '/entrar',
+	REGISTER_URL: '/registro',
+	AUTH_USUARIOS_URL: '/auth/usuarios',
+	AUTH_ANUNCIOS_URL: '/auth/anuncios',
+	AUTH_PETS_URL: '/auth/pets',
+};
+
+export default routes;
