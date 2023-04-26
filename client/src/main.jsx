cae312dbe47585feb13660718c77fb51f6ca22ed
Alisson Silva dos Santos <92067558+alissons-repos@users.noreply.github.com>
2023-04-26 10:06:25 -0300

    atualizando o arquivo main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { AuthProvider } from './context/AuthProvider';
+
+import App from './App';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<BrowserRouter>
+			<AuthProvider>
+				<Routes>
+					<Route path='/*' element={<App />} />
+				</Routes>
+			</AuthProvider>
+		</BrowserRouter>
+	</React.StrictMode>
+);
