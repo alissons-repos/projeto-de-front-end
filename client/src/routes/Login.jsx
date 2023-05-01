f05c94372f1dc4b20dc73050189ca53ba9fd82fa
Alisson Silva dos Santos <92067558+alissons-repos@users.noreply.github.com>
2023-05-01 17:49:34 -0300

    estilizando rota login

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -77,43 +77,104 @@ const Login = () => {
 					{navigate('/feed')};
 				</section>
 			) : (
-				<section>
-					<p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'}>
-						{errMsg}
-					</p>
-					<h1>Entrar</h1>
-					<form onSubmit={handleSubmit}>
-						<label htmlFor='email'>E-mail:</label>
-						<input
-							type='email'
-							id='email'
-							autoComplete='off'
-							ref={userRef}
-							value={user} // Torna o valor do input controlável e assim podemos limpar o campo quando necessário
-							required
-							onChange={(e) => setUser(e.target.value)}
-						/>
-						<label htmlFor='password'>Senha:</label>
-						<input
-							type='password'
-							id='password'
-							value={pwd}
-							required
-							onChange={(e) => setPwd(e.target.value)}
-						/>
-						<button type='submit'>Entrar</button>
-						<div className='persistCheck'>
-							<input type='checkbox' id='persist' onChange={togglePersist} checked={persist} />
-							<label htmlFor='persist'>Manter-se conectado</label>
+				<section className='container-fluid'>
+					{/* Página inteira */}
+					<div className='row'>
+						{/* Direita */}
+						<div className='col-12 col-lg-7 vh-100 px-4 text-white' style={{ backgroundColor: '#ffbe5c' }}>
+							<div className='d-flex flex-column justify-content-center h-100'>
+								<p className='lead fs-1'>Arranje um companheiro para o seu Pet!</p>
+								<hr className='py-4' />
+								<p className='fs-5'>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis incidunt sed quisquam
+									voluptates aliquam quas nostrum, ipsa minus in voluptatum animi id quos esse quia
+									voluptate beatae. Cupiditate, laborum dolorum?
+								</p>
+								<Link className='btn btn-outline-light btn-lg' to='/register'>
+									Faça parte
+								</Link>
+							</div>
 						</div>
-					</form>
-					<p>
-						Ainda não possui uma conta?
-						<br />
-						<span className='line'>
-							<Link to='/register'>Cadastre-se</Link>
-						</span>
-					</p>
+						{/* Esquerda */}
+						<div className='col-12 col-lg-5 vh-100 px-4 text-white' style={{ backgroundColor: '#f2f2f2' }}>
+							<div
+								className='d-flex flex-column justify-content-center h-100'
+								style={{ color: '#909090' }}
+							>
+								<h1 className='text-center display-2' style={{ color: '#ffbe5c' }}>
+									Meu Amigo PET
+								</h1>
+								<form onSubmit={handleSubmit}>
+									<div className='px-2 mb-3'>
+										<label className='form-label' htmlFor='email'>
+											E-mail:
+										</label>
+										<input
+											className='form-control'
+											placeholder='emailbacana@mail.com'
+											type='email'
+											id='email'
+											autoComplete='off'
+											ref={userRef}
+											value={user} // Torna o valor do input controlável e assim podemos limpar o campo quando necessário
+											required
+											onChange={(e) => setUser(e.target.value)}
+										/>
+										<div className='form-text' style={{ color: '#909090' }} id='emailHelp'>
+											Não compartilharemos seu email
+										</div>
+									</div>
+									<div className='px-2 mb-3'>
+										<label className='form-label' htmlFor='password'>
+											Senha:
+										</label>
+										<input
+											className='form-control'
+											placeholder='**********'
+											type='password'
+											id='password'
+											value={pwd}
+											required
+											onChange={(e) => setPwd(e.target.value)}
+										/>
+										<div className='form-text' style={{ color: '#909090' }} id='passwordHelp'>
+											Utilize uma senha forte
+										</div>
+									</div>
+									<div className='px-2 mb-3'>
+										<div className='persistCheck'>
+											<input
+												className='me-2'
+												type='checkbox'
+												id='persist'
+												onChange={togglePersist}
+												checked={persist}
+											/>
+											<label htmlFor='persist'>Manter-se conectado</label>
+										</div>
+									</div>
+									<div className='px-2 mb-3 d-flex flex-column gap-4'>
+										<button
+											className='btn btn-secondary'
+											style={{ backgroundColor: '#ffbe5c', border: 'none' }}
+											type='submit'
+										>
+											Entrar
+										</button>
+										<span className='text-center'>
+											<Link to='/recover'>Esqueci minha senha</Link>
+										</span>
+										<p className='text-center'>
+											Ainda não possui uma conta? <Link to='/register'>Cadastre-se</Link>
+										</p>
+									</div>
+								</form>
+								<p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'}>
+									{errMsg}
+								</p>
+							</div>
+						</div>
+					</div>
 				</section>
 			)}
 		</>
