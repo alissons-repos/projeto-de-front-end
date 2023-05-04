90dee1c73ad163596f29b0727254bf4563878e70
Alisson Silva dos Santos <92067558+alissons-repos@users.noreply.github.com>
2023-05-04 11:20:12 -0300

    atualizando middlewares e criando arquivos de models

--- server/src/middlewares/verifyID.js
+++ server/src/middlewares/verifyID.js
@@ -1,10 +1,9 @@
 const { validate: isUuid } = require('uuid');
 
 const verifyID = async (req, res, next) => {
+	if (!req?.params?.id) return res.status(400).json({ Erro: 'ID não informado!' }); // Bad Request
 	const index = req.params.id;
-
-	if (!isUuid(index)) return res.status(400).json({ Erro: 'ID inválido!' });
-
+	if (!isUuid(index)) return res.status(400).json({ Erro: 'ID inválido!' }); // Bad Request
 	next();
 };
 
