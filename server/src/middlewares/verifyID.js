5ddb436995f28e418f195b75dc09f7f0eac4f119
Alisson Silva dos Santos <92067558+alissons-repos@users.noreply.github.com>
2023-05-08 11:30:07 -0300

    alterando middlewares para se adequarem as rotas e controllers

--- server/src/middlewares/verifyID.js
+++ server/src/middlewares/verifyID.js
@@ -1,9 +1,11 @@
-const { validate: isUuid } = require('uuid');
+const mongoose = require('mongoose');
 
 const verifyID = async (req, res, next) => {
 	if (!req?.params?.id) return res.status(400).json({ Erro: 'ID não informado!' }); // Bad Request
-	const index = req.params.id;
-	if (!isUuid(index)) return res.status(400).json({ Erro: 'ID inválido!' }); // Bad Request
+	const validID = mongoose.Types.ObjectId.isValid(req.params.id);
+	// const id = req.params.id;
+	// const result = id.match(/^[0-9a-fA-F]{24}$/);
+	if (!validID) return res.status(400).json({ Erro: 'ID inválido!' }); // Bad Request
 	next();
 };
 
--- server/src/middlewares/verifyJWT.js
+++ server/src/middlewares/verifyJWT.js
@@ -3,17 +3,16 @@ const jwt = require('jsonwebtoken');
 const verifyJWT = (req, res, next) => {
 	const authHeader = req.headers.authorization || req.headers.Authorization;
 
-	if (!authHeader?.startsWith('Bearer ')) return res.status(401).json({ Erro: 'Token não informado!' });
+	if (!authHeader?.startsWith('Bearer ')) return res.status(401).json({ Erro: 'Token não informado!' }); // Unauthorized
 
 	const token = authHeader.split(' ')[1]; // Estamos dividindo ao meio, onde há um espaço em branco, e pegando a segunda parte
 
 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
 		if (error) return res.status(403).json({ Erro: 'Token inválido!' }); // Forbidden (proibido)
-		req.user = decoded.userData.username;
-		// Estamos agregando um nome de usuário a requisição, assim teremos acesso fácil a esse dado
+		req.user = decoded.userData;
+		// Estamos agregando um usuário a requisição
+		next();
 	});
-
-	next();
 };
 
 module.exports = verifyJWT;
