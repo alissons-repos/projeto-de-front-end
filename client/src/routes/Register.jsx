1ec86c39a30e085062f916370de1aa40fbebbdf4
Alisson Silva dos Santos <92067558+alissons-repos@users.noreply.github.com>
2023-05-11 11:23:47 -0300

    implementando rota Register

--- client/src/routes/Register.jsx
+++ client/src/routes/Register.jsx
@@ -7,7 +7,7 @@ import path from '../apis/endpoints';
 
 import style from './Register.module.css';
 
-const USER_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,4})+$/;
+const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,4})+$/;
 const PWD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[?!@#$%&*,.;:/]).{8,}$/;
 
 const Register = () => {
@@ -15,12 +15,18 @@ const Register = () => {
 	const location = useLocation();
 	const from = location.state?.from?.pathname || '/';
 
-	const userRef = useRef(); // Necessário para por o foco no campo e-mail quando a tela recarregar
+	const emailRef = useRef(); // Necessário para por o foco no campo e-mail quando a tela recarregar
 	const errRef = useRef(); // Necessário para por o foco em erros para auxiliar na acessibilidade
 
-	const [user, setUser] = useState('');
-	const [validName, setValidName] = useState(false);
-	const [userFocus, setUserFocus] = useState(false);
+	const [firstName, setFirstName] = useState('');
+	const [firstNameFocus, setFirstNameFocus] = useState(false);
+
+	const [lastName, setLastName] = useState('');
+	const [lastNameFocus, setLastNameFocus] = useState(false);
+
+	const [email, setEmail] = useState('');
+	const [validEmail, setValidEmail] = useState(false);
+	const [emailFocus, setEmailFocus] = useState(false);
 
 	const [pwd, setPwd] = useState('');
 	const [validPwd, setValidPwd] = useState(false);
@@ -34,12 +40,12 @@ const Register = () => {
 	const [success, setSuccess] = useState(false);
 
 	useEffect(() => {
-		userRef.current.focus(); // Por o foco no input de email
+		emailRef.current.focus(); // Por o foco no input de email
 	}, []);
 
 	useEffect(() => {
-		setValidName(USER_REGEX.test(user));
-	}, [user]);
+		setValidEmail(EMAIL_REGEX.test(email));
+	}, [email]);
 
 	useEffect(() => {
 		setValidPwd(PWD_REGEX.test(pwd));
@@ -47,26 +53,26 @@ const Register = () => {
 	}, [pwd, matchPwd]);
 
 	useEffect(() => {
-		setErrMsg(''); // Limpar o erro sempre que houver alguma alteração em "user" e "pwd" e "matchPwd"
-	}, [user, pwd, matchPwd]);
+		setErrMsg(''); // Limpar o erro sempre que houver alguma alteração em "email" e "pwd" e "matchPwd"
+	}, [email, pwd, matchPwd]);
 
-	const handleSubmit = async (e) => {
-		e.preventDefault(); // Previne o comportamento padrão do formulário ao recarregar a página
-		const v1 = USER_REGEX.test(user);
+	const handleSubmit = async (event) => {
+		event.preventDefault(); // Previne o comportamento padrão do formulário ao recarregar a página
+		const v1 = EMAIL_REGEX.test(email);
 		const v2 = PWD_REGEX.test(pwd);
 		if (!v1 || !v2) {
 			setErrMsg('E-mail ou senha inválido!');
 			return;
 		}
 		try {
-			const response = await api.post(path.REGISTER_URL, JSON.stringify({ user, pwd }), {
+			const response = await api.post(path.REGISTER_URL, JSON.stringify({ email, pwd }), {
 				withCredentials: true,
 			});
 			console.log(response?.data); // TODO: COMENTAR A LINHA QUANDO ESTIVER PRONTO
 			console.log(response?.accessToken); // TODO: COMENTAR A LINHA QUANDO ESTIVER PRONTO
 			console.log(JSON.stringify(response)); // TODO: COMENTAR A LINHA QUANDO ESTIVER PRONTO
 			setSuccess(true);
-			setUser('');
+			setEmail('');
 			setPwd('');
 			setMatchPwd('');
 			navigate(from, { replace: true });
@@ -91,82 +97,151 @@ const Register = () => {
 					{navigate('/login')};
 				</section>
 			) : (
-				<section>
-					<p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'}>
-						{errMsg}
-					</p>
-					<h1>Cadastre-se</h1>
-					<form onSubmit={handleSubmit}>
-						<label htmlFor='username'>
-							E-mail:
-							<FaCheck className={validName ? 'valid' : 'hide'} />
-							<FaTimes className={validName || !user ? 'hide' : 'invalid'} />
-						</label>
-						<input
-							type='text'
-							id='username'
-							ref={userRef}
-							autoComplete='off'
-							onChange={(e) => setUser(e.target.value)}
-							value={user}
-							required
-							onFocus={() => setUserFocus(true)}
-							onBlur={() => setUserFocus(false)}
-						/>
-						<p id='uidnote' className={userFocus && user && !validName ? 'instructions' : 'offscreen'}>
-							<FaInfoCircle />
-							Deve ser um e-mail válido.
-							{/* TODO: MELHORAR A DESCRIÇÃO DE ERRO DO E-MAIL */}
-						</p>
+				<section className='container'>
+					<h1 className='text-center'>Cadastre-se</h1>
+					<form onSubmit={handleSubmit} className='row'>
+						<div className='col-12 my-2'>
+							<label htmlFor='email' className='form-label'>
+								E-mail:
+								<FaCheck className={validEmail ? 'valid' : 'hide'} />
+								<FaTimes className={validEmail || !email ? 'hide' : 'invalid'} />
+							</label>
+							<input
+								className='form-control'
+								placeholder='emailbacana@mail.com'
+								type='email'
+								id='email'
+								ref={emailRef}
+								autoComplete='off'
+								onChange={(event) => setEmail(event.target.value)}
+								value={email}
+								required
+								onFocus={() => setEmailFocus(true)}
+								onBlur={() => setEmailFocus(false)}
+							/>
+							<div className='form-text' style={{ color: '#909090' }} id='emailHelp'>
+								Não compartilharemos seu email com ninguém
+							</div>
+							<p
+								id='uidnote'
+								className={emailFocus && email && !validEmail ? 'instructions' : 'offscreen'}
+							>
+								<FaInfoCircle />
+								Deve ser um e-mail válido.
+								{/* TODO: MELHORAR A DESCRIÇÃO DE ERRO DO E-MAIL */}
+							</p>
+						</div>
 
-						<label htmlFor='password'>
-							Senha:
-							<FaCheck className={validPwd ? 'valid' : 'hide'} />
-							<FaTimes className={validPwd || !pwd ? 'hide' : 'invalid'} />
-						</label>
-						<input
-							type='password'
-							id='password'
-							onChange={(e) => setPwd(e.target.value)}
-							value={pwd}
-							required
-							onFocus={() => setPwdFocus(true)}
-							onBlur={() => setPwdFocus(false)}
-						/>
-						<p id='pwdnote' className={pwdFocus && !validPwd ? 'instructions' : 'offscreen'}>
-							<FaInfoCircle />A senha deve conter 8 ou mais caracteres, pelo menos uma letra maiúscula,
-							uma minúscula, um número e um símbolo especial.
-							<br />
-							Símbolos especiais permitidos: ? ! @ # $ % & * , . ; : /
-						</p>
+						<div className='col-12 col-lg-6 my-2'>
+							<label htmlFor='password' className='form-label'>
+								Senha:
+								<FaCheck className={validPwd ? 'valid' : 'hide'} />
+								<FaTimes className={validPwd || !pwd ? 'hide' : 'invalid'} />
+							</label>
+							<input
+								className='form-control'
+								placeholder='**********'
+								type='password'
+								id='password'
+								onChange={(event) => setPwd(event.target.value)}
+								value={pwd}
+								required
+								onFocus={() => setPwdFocus(true)}
+								onBlur={() => setPwdFocus(false)}
+							/>
+							<div className='col-12 form-text' style={{ color: '#909090' }} id='passwordHelp'>
+								A senha deve conter 8 ou mais caracteres, pelo menos uma letra maiúscula, uma minúscula,
+								um número e um símbolo especial. Símbolos especiais permitidos: ? ! @ # $ % & * , . ; :
+								/
+							</div>
+							<p
+								id='pwdnote'
+								className={pwdFocus && !validPwd ? 'form-text instructions' : 'form-text offscreen'}
+							>
+								<FaInfoCircle />A senha deve conter 8 ou mais caracteres, pelo menos uma letra
+								maiúscula, uma minúscula, um número e um símbolo especial. Símbolos especiais
+								permitidos: ? ! @ # $ % & * , . ; : /
+							</p>
+						</div>
 
-						<label htmlFor='confirm_pwd'>
-							Confirmar Senha:
-							<FaCheck className={validMatch && matchPwd ? 'valid' : 'hide'} />
-							<FaTimes className={validMatch || !matchPwd ? 'hide' : 'invalid'} />
-						</label>
-						<input
-							type='password'
-							id='confirm_pwd'
-							onChange={(e) => setMatchPwd(e.target.value)}
-							value={matchPwd}
-							required
-							onFocus={() => setMatchFocus(true)}
-							onBlur={() => setMatchFocus(false)}
-						/>
-						<p id='confirmnote' className={matchFocus && !validMatch ? 'instructions' : 'offscreen'}>
-							<FaInfoCircle />A confirmação deve coincidir com o campo senha.
-						</p>
+						<div className='col-12 col-lg-6 my-2'>
+							<label htmlFor='confirm_pwd' className='form-label'>
+								Confirmar Senha:
+								<FaCheck className={validMatch && matchPwd ? 'valid' : 'hide'} />
+								<FaTimes className={validMatch || !matchPwd ? 'hide' : 'invalid'} />
+							</label>
+							<input
+								className='form-control'
+								placeholder='**********'
+								type='password'
+								id='confirm_pwd'
+								onChange={(event) => setMatchPwd(event.target.value)}
+								value={matchPwd}
+								required
+								onFocus={() => setMatchFocus(true)}
+								onBlur={() => setMatchFocus(false)}
+							/>
+							<p id='confirmnote' className={matchFocus && !validMatch ? 'instructions' : 'offscreen'}>
+								<FaInfoCircle />A confirmação deve coincidir com o campo senha.
+							</p>
+						</div>
 
-						<button disabled={!validName || !validPwd || !validMatch ? true : false}>Registre-se</button>
+						<div className='col-12 col-lg-6 my-2'>
+							<label htmlFor='firstName' className='form-label'>
+								Nome:
+							</label>
+							<input
+								className='form-control'
+								placeholder='Fulano Beltrano'
+								type='text'
+								id='firstName'
+								onChange={(event) => setFirstName(event.target.value)}
+								value={firstName}
+								required
+								onFocus={() => setFirstNameFocus(true)}
+								onBlur={() => setFirstNameFocus(false)}
+							/>
+							<div className='form-text' style={{ color: '#909090' }} id='passwordHelp'>
+								Pode utilizar também nomes compostos
+							</div>
+						</div>
+
+						<div className='col-12 col-lg-6 my-2'>
+							<label htmlFor='lastName' className='form-label'>
+								Sobrenome:
+							</label>
+							<input
+								className='form-control'
+								placeholder='Sicrano de Souza'
+								type='text'
+								id='lastName'
+								onChange={(event) => setLastName(event.target.value)}
+								value={lastName}
+								required
+								onFocus={() => setLastNameFocus(true)}
+								onBlur={() => setLastNameFocus(false)}
+							/>
+						</div>
+
+						<div className='my-3 d-flex flex-column'>
+							<button
+								className='btn btn-secondary'
+								style={{ backgroundColor: '#ffbe5c', border: 'none' }}
+								type='submit'
+								disabled={!validEmail || !validPwd || !validMatch ? true : false}
+							>
+								Registre-se
+							</button>
+						</div>
 					</form>
-					<p>
-						Já possui um cadastro?
-						<br />
-						<span className='line'>
-							<Link to='/login'>Entrar</Link>
-						</span>
-					</p>
+					<section>
+						<p className='text-center'>
+							Já possui um cadastro? <Link to='/login'>Entrar</Link>
+						</p>
+						<p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'}>
+							{errMsg}
+						</p>
+					</section>
 				</section>
 			)}
 		</>
