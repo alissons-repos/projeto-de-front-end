ef205a296b7afc665159fc78e853cbaee2de8340
Alisson Silva dos Santos <92067558+alissons-repos@users.noreply.github.com>
2023-05-24 11:27:17 -0300

    integrando backend ao componente Profile 1 #31

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,12 +2,7 @@ import { Outlet } from 'react-router-dom'; // Outlet representa todos os filhos 
 
 const Layout = () => {
 	// Não é necessário passar o atributo "children" dentro dos parâmetro do componente
-	return (
-		<main className=''>
-			<Outlet />
-			{/* <Footer /> */}
-		</main>
-	);
+	return <Outlet />;
 };
 
 export default Layout;
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,48 +1,99 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import useAuth from '../hooks/useAuth';
+import useApiPrivate from '../hooks/useApiPrivate';
+import path from '../apis/endpoints';
 
 const Profile = () => {
-	const user = { firstName: 'Fulano', lastName: 'Sicrano Beltrano' };
+	const { auth } = useAuth();
+	const [active, setActive] = useState(false);
+	const [currentUser, setCurrentUser] = useState(auth.userData);
+	const imagePath = `${path.BASE_URL}${path.PUBLIC_URL}/${auth.userData.avatar}`;
+
 	const handleSubmit = () => {};
+
+	useEffect(() => {
+		console.log(auth);
+	}, []);
 
 	return (
 		<div className='col-12 col-xl-10'>
 			<div className='row mt-5'>
 				<h1 className='text-center display-4 pt-4 mb-3'>Meu Perfil</h1>
-				<form onSubmit={handleSubmit} className='row px-5'>
-					<div className='col-12 col-lg-6 my-2'>
-						<label htmlFor='firstName' className='form-label'>
-							Nome:
-						</label>
-						<input type='text' className='form-control' value={user.firstName} readOnly />
-					</div>
-
-					<div className='col-12 col-lg-6 my-2'>
-						<label htmlFor='lastName' className='form-label'>
-							Sobrenome:
-						</label>
-						<input type='text' className='form-control' value={user.lastName} readOnly />
-					</div>
-
-					<div className='col-12 my-2'>
-						<label htmlFor='email' className='form-label'>
-							E-mail:
-						</label>
-						<input type='email' className='form-control' />
-					</div>
-
-					{/* <div className='d-flex my-2 justify-content-end'>
-						<button
-							className='btn btn-secondary'
-							style={{ backgroundColor: '#fe9a2e', border: 'none' }}
-							type='submit'
-							disabled={false ? true : false}
-						>
-							Alterar Dados
-						</button>
-					</div> */}
-				</form>
 
 				<div className='row d-flex flex-column flex-lg-row px-5'>
+					<div className='w-50 w-lg-50'>
+						<form onSubmit={handleSubmit} className=''>
+							<div className='col-12 my-2'>
+								<label htmlFor='firstName' className='form-label'>
+									Nome:
+								</label>
+								<input
+									type='text'
+									className='form-control text-capitalize'
+									value={currentUser.firstName}
+									readOnly
+								/>
+							</div>
+
+							<div className='col-12 my-2'>
+								<label htmlFor='lastName' className='form-label'>
+									Sobrenome:
+								</label>
+								<input
+									type='text'
+									className='form-control text-capitalize'
+									value={currentUser.lastName}
+									readOnly
+								/>
+							</div>
+
+							<div className='col-12 my-2'>
+								<label htmlFor='email' className='form-label'>
+									E-mail:
+								</label>
+								<input
+									type='email'
+									className='form-control text-lowercase'
+									value={currentUser.email}
+									readOnly
+								/>
+							</div>
+						</form>
+					</div>
+
+					<div className='w-50'>
+						<form onSubmit={handleSubmit} className=''>
+							<fieldset className='row'>
+								<div className='col-12 my-2'>
+									<label htmlFor='avatar' className='form-label'>
+										Avatar:
+									</label>
+									<input type='file' className='form-control' />
+								</div>
+								<div className='d-flex justify-content-center'>
+									<img
+										src={imagePath}
+										alt=''
+										className='bg-dark w-50'
+										style={{ width: '100%', height: '100%' }}
+									/>
+								</div>
+							</fieldset>
+
+							<div className='my-2'>
+								<button
+									className='btn btn-secondary'
+									style={{ backgroundColor: '#fe9a2e', border: 'none' }}
+									type='submit'
+									disabled={false ? true : false}
+								>
+									Alterar Foto de Perfil
+								</button>
+							</div>
+						</form>
+					</div>
+
 					<div className='w-50'>
 						<form onSubmit={handleSubmit} className=''>
 							<fieldset className='row'>
@@ -76,30 +127,6 @@ const Profile = () => {
 									disabled={false ? true : false}
 								>
 									Alterar Senha
-								</button>
-							</div>
-						</form>
-					</div>
-
-					<div className='w-50'>
-						<form onSubmit={handleSubmit} className=''>
-							<fieldset className='row'>
-								<div className='col-12 my-2'>
-									<label htmlFor='avatar' className='form-label'>
-										Avatar:
-									</label>
-									<input type='file' className='form-control' />
-								</div>
-							</fieldset>
-
-							<div className='my-2'>
-								<button
-									className='btn btn-secondary'
-									style={{ backgroundColor: '#fe9a2e', border: 'none' }}
-									type='submit'
-									disabled={false ? true : false}
-								>
-									Alterar Foto de Perfil
 								</button>
 							</div>
 						</form>
