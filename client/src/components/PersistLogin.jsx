fb5f76f91430860b4e9be99f8cb1539497605530
Alisson Silva dos Santos <92067558+alissons-repos@users.noreply.github.com>
2023-06-01 10:49:40 -0300

    resolvendo a persistência do usuário logado resolved #32

--- client/src/components/PersistLogin.jsx
+++ client/src/components/PersistLogin.jsx
@@ -3,19 +3,20 @@ import { Outlet } from 'react-router-dom';
 
 import useAuth from '../hooks/useAuth';
 import useRefresh from '../hooks/useRefreshToken';
-// import useApiPrivate from '../hooks/useApiPrivate';
-// import path from '../apis/endpoints';
+import useApiPrivate from '../hooks/useApiPrivate';
+import path from '../apis/endpoints';
 
 const PersistLogin = () => {
 	const { auth, setAuth, persist } = useAuth();
 	const refresh = useRefresh();
-	// const apiPrivate = useApiPrivate();
+	const apiPrivate = useApiPrivate();
 
-	const [isLoading, setIsLoading] = useState(true);
-	// const persistRefresh = localStorage.getItem('refresh');
+	const [isLoadingToken, setIsLoadingToken] = useState(true);
+	const [isLoadingData, setIsLoadingData] = useState(true);
+	const persistRefresh = localStorage.getItem('refresh');
 
 	useEffect(() => {
-		let isMounted = true; // Utilizado para resolver o problema de memory liking
+		let isMounted = true; // Utilizado para resolver o problema de memory leaking
 
 		const verifyRefreshToken = async () => {
 			try {
@@ -24,25 +25,28 @@ const PersistLogin = () => {
 			} catch (error) {
 				console.error(error); // TODO: COMENTAR A LINHA QUANDO ESTIVER PRONTO
 			} finally {
-				isMounted && setIsLoading(false);
-				// Estrutura condicional para resolver o problema de memory liking
+				isMounted && setIsLoadingToken(true);
+				// setIsLoadingToken(false);
+				// Estrutura condicional para resolver o problema de memory leaking
 			}
 		};
 
-		// const persistUserData = async () => {
-		// 	try {
-		// 		const resGetUsers = await apiPrivate.get(path.USER_URL);
-		// 		const userData = resGetUsers?.data?.find((user) => user.refreshToken === persistRefresh);
-		// 		setAuth((previous) => ({ ...previous, userData }));
-		// 	} catch (error) {
-		// 		console.error(error);
-		// 	} finally {
-		// 		isMounted && setIsLoading(false);
-		// 	}
-		// };
+		const persistUserData = async () => {
+			try {
+				const resGetUsers = await apiPrivate.get(path.USER_URL);
+				const userData = resGetUsers?.data?.find((user) => user.refreshToken === persistRefresh);
+				setAuth((previous) => ({ ...previous, userData }));
+			} catch (error) {
+				console.error(error);
+			} finally {
+				isMounted && setIsLoadingData(true);
+				// setIsLoadingData(false);
+			}
+		};
 
 		// !auth?.userData ? persistUserData() : setAuth((previous) => previous);
-		!auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+		!auth?.userData ? persistUserData() : setIsLoadingData(true);
+		!auth?.accessToken ? verifyRefreshToken() : setIsLoadingToken(true);
 		// A função verifyRefreshToken() deve ser executada apenas uma vez ao recarregar a página, pois se houver um usuário logado e seu token estiver válido, não é necessário ir buscar um novo refreshtoken na rota "/refresh"
 
 		return () => {
@@ -50,7 +54,17 @@ const PersistLogin = () => {
 		};
 	}, []);
 
-	return <>{!persist ? <Outlet /> : isLoading ? <p>Carregando...</p> : <Outlet />}</>;
+	return (
+		<>
+			{!persist ? (
+				<Outlet />
+			) : isLoadingToken || isLoadingData ? (
+				<h1 className='vh-100 d-flex justify-content-center align-items-center display-6'>Carregando...</h1>
+			) : (
+				<Outlet />
+			)}
+		</>
+	);
 	// Outlet renderizará todos os componentes filhos dentro do componente PersistLogin
 };
 
